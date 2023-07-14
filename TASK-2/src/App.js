759fd57c3d6385ee3cfe6f5632916a009d86b4e9
Sristi   Kundu <123077445+sristikundu895@users.noreply.github.com>
2023-07-14 22:05:06 +0530

    Add files via upload

---
+++ TASK-2/src/App.js
@@ -0,0 +1,53 @@
+import Users from "./Components/card";
+import './App.css';
+
+import React, { Component } from 'react'
+	
+class App extends Component {
+  constructor(props){
+	super(props)
+		
+	// Set initial state
+	this.state = {users_data :[],
+                loading : true                
+  }
+  
+	// Binding this keyword
+	this.updateState = this.updateState.bind(this)
+  }
+    
+  updateState(){
+      console.log("VT");
+      document.getElementById("main").style.display='flex';
+      const link="https://reqres.in/api/users?page=1";
+      fetch(link)
+      .then(response => response.json())
+      .then((users) => {
+        
+        this.setState({users_data :users.data,
+                        loading: false
+        })
+        console.log(users.data);
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+  }
+    
+  render(){
+    return (<>
+      <nav className="navbar navbar-expand-lg navbar-light">
+          <div className="container-fluid">
+            <h3>LGM APP</h3> 
+            <p><h1>Welcome to Our Community</h1></p>
+            <button onClick={this.updateState}>Get Users</button>
+          </div>
+        </nav>
+      
+      <Users loading={this.state.loading} users={this.state.users_data}/>
+    </>
+    )
+  }
+}
+	
+export default App;
